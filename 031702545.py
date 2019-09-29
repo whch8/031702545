e8c0dfe9d1a853926bb806c58d0e540276c9d244
Ignorant <1277950703@qq.com>
2019-09-29 17:04:36 +0800

    add py

--- 031702545.py
+++ 031702545.py
@@ -3,82 +3,101 @@
 import re
 import json
 
-cache = input()
+while 1:
+    addr=input()
+    if addr == "END":
+        break
+    key={}
+    result=[]
 
-dict = {}
-list = []
+    addr=re.sub('\d!','',addr)
 
-cache = re.sub('\d!','',cache)
+    name=re.search('.+,', addr).group()  #提取名字
+    name=re.search('.+,', addr).group() 
+    key['姓名']=re.search('[^,]+', name).group()
+    key['姓名']=re.search('[^,]+', name).group()
+    addr=re.sub('.+,', '', addr)
 
-name = re.search('.+,', cache).group()  #提取名字
-dict['姓名'] = re.search('[^,]+', name).group()
-cache = re.sub('.+,', '', cache)
+    key['手机']=re.search('\d{11}', addr).group()   #提取号码
+    key['手机']=re.search('\d{11}', addr).group()
+    addr=re.sub('\d{11}', '', addr)
 
-dict['手机']= re.search('\d{11}', cache).group()   #提取号码
-cache = re.sub('\d{11}', '', cache)
+    point=re.search('.{2}', addr).group()  #省
+    point=re.search('.{2}', addr).group()
+    flag=re.search('.+?省', addr)
+    flag=re.search('.+?省', addr)
+    if point =='天津' or point =='北京' or point =='上海' or point =='重庆' :
+        result.append(point)
+    elif flag!=None :
+        point=re.search('.+?省', addr).group()
+        point=re.search('.+?省', addr).group()
+        addr=addr.replace(point, '', 1)
+        result.append(point)
+    else:
+        point=re.search('..', addr).group()
+        addr=addr.replace(point, '', 1)
+        result.append(point+'省')
+    if point =='天津' or point =='北京' or point =='上海' or point =='重庆' :
+        pass
 
-aaa = re.search('.{2}', cache).group()  #省
-flag = re.search('.+?省', cache)
-if aaa == '北京' or aaa == '天津' or aaa == '重庆' or aaa == '上海' :
-    list.append(aaa)
-elif flag != None :
-    aaa = re.search('.+?省', cache).group()
-    cache = cache.replace(aaa, '', 1)
-    list.append(aaa)
-else:
-    aaa = re.search('..', cache).group()
-    cache = cache.replace(aaa, '', 1)
-    list.append(aaa+'省')
+    flag=re.search('.+?市', addr)  #市
+    if flag!=None :
+        point=re.search('.+?市', addr).group()
+        addr=addr.replace(point, '', 1)
+        result.append(point)
+    else:
+        point=re.search('..', addr).group()
+        addr=addr.replace(point, '', 1)
+        result.append(point+'市')
+    if flag!=None :
+        pass
 
-flag = re.search('.+?市', cache)  #市
-if flag != None :
-    aaa = re.search('.+?市', cache).group()
-    cache = cache.replace(aaa, '', 1)
-    list.append(aaa)
-else:
-    aaa = re.search('..', cache).group()
-    cache = cache.replace(aaa, '', 1)
-    list.append(aaa+'市')
+    flag=re.search('.+?(?:县|区)', addr)  # 县/区
+    flag=re.search('.+?(?:县|区)', addr)
+    if flag!=None :
+        point=re.search('.+?(?:县|区)', addr).group()
+        addr=addr.replace(point, '', 1)
+        result.append(point)
+    else:
+        result.append('')
 
-flag = re.search('.+?(?:县|区)', cache)  #县/区
-if flag != None :
-    aaa = re.search('.+?(?:县|区)', cache).group()
-    cache = cache.replace(aaa, '', 1)
-    list.append(aaa)
-else:
-    list.append('')
+    flag=re.search('..+?(?:镇|街道)', addr)  #镇/街道/乡
+    if flag!=None :
+        point=re.search('..+?(?:镇|街道|乡)', addr).group()
+        point=re.search('..+?(?:镇|街道|乡)', addr).group()
+        addr=addr.replace(point, '', 1)
+        addr=addr.replace(point, '', 1)
+        result.append(point)
+    else:
+        result.append('')
+    if flag!=None :
+       pass
 
-flag = re.search('..+?(?:镇|街道)', cache)  #镇/街道/乡
-if flag != None :
-    aaa = re.search('..+?(?:镇|街道|乡)', cache).group()
-    cache = cache.replace(aaa, '', 1)
-    list.append(aaa)
-else:
-    list.append('')
+    flag=re.search('.+?(?:路|巷|街|道|乡)', addr)  #路/巷/街
+    if flag!=None :
+        point=re.search('.+?(?:路|巷|街|道|乡)', addr).group()
+        addr=addr.replace(point, '', 1)
+        result.append(point)
+    else:
+        result.append('')
 
-flag = re.search('.+?(?:路|巷|街|道|乡)', cache)  #路/巷/街
-if flag != None :
-    aaa = re.search('.+?(?:路|巷|街|道|乡)', cache).group()
-    cache = cache.replace(aaa, '', 1)
-    list.append(aaa)
-else:
-    list.append('')
+    flag=re.search('.+?(?:号|村)', addr)  #号
+    flag=re.search('.+?(?:号|村)', addr)
+    if (flag!=None) :
+        point=re.search('.+?(?:号|村)', addr).group()
+        addr=addr.replace(point, '', 1)
+        result.append(point)
+    else:
+        result.append('')
 
-flag = re.search('.+?(?:号|村)', cache)  #号
-if (flag != None) :
-    aaa = re.search('.+?(?:号|村)', cache).group()
-    cache = cache.replace(aaa, '', 1)
-    list.append(aaa)
-else:
-    list.append('')
+    flag=re.search('[^\.]+', addr)  #具体地址
+    if flag!=None :
+        point=re.search('[^\.]+', addr).group()
+        point=re.search('[^\.]+', addr).group()
+        result.append(point)
+    else:
+        result.append('')
 
-flag = re.search('[^\.]+', cache)  #具体地址
-if flag != None :
-    aaa = re.search('[^\.]+', cache).group()
-    list.append(aaa)
-else:
-    list.append('')
-
-dict['地址'] = list
-j = json.dumps(dict,ensure_ascii=False)
-print(j)  #结束
+    key['地址']=result
+    finresult=json.dumps(key,ensure_ascii=False)
+    print(finresult)  #结束
